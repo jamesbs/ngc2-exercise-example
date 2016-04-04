73f9132d4bfc3fb4c59614d7955a24c126ce642d
James Salas <j@jamesbs.com>
2016-04-04 00:29:50 -0400

    upgrade angular2 package

--- jspm.config.js
+++ jspm.config.js
@@ -42,12 +42,12 @@ SystemJS.config({
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "ts": "github:frankwallis/plugin-typescript@4.0.5",
     "typescript": "jspm_packages/npm/typescript@1.8.9%2Flib%2Ftypescript.js",
-    "angular2": "npm:angular2@2.0.0-beta.12",
+    "angular2": "npm:angular2@2.0.0-beta.13",
     "es6-promise": "npm:es6-promise@3.1.2",
     "es6-shim": "npm:es6-shim@0.33.13",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
     "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.5",
-    "reflect-metadata": "npm:reflect-metadata@0.1.3",
+    "reflect-metadata": "npm:reflect-metadata@0.1.2",
     "rxjs": "npm:rxjs@5.0.0-beta.2",
     "zone.js": "npm:zone.js@0.6.6"
   },
@@ -62,27 +62,13 @@ SystemJS.config({
         "os-browserify": "npm:os-browserify@0.2.1"
       }
     },
-    "npm:angular2@2.0.0-beta.12": {
-      "map": {
-        "es6-shim": "npm:es6-shim@0.35.0",
-        "reflect-metadata": "npm:reflect-metadata@0.1.3",
-        "rxjs": "npm:rxjs@5.0.0-beta.2",
-        "zone.js": "npm:zone.js@0.6.6"
-      }
-    },
     "npm:es6-promise@3.1.2": {
       "map": {}
     },
     "npm:es6-shim@0.33.13": {
       "map": {}
     },
-    "npm:es6-shim@0.35.0": {
-      "map": {}
-    },
     "npm:reflect-metadata@0.1.2": {
-      "map": {}
-    },
-    "npm:reflect-metadata@0.1.3": {
       "map": {}
     },
     "npm:rxjs@5.0.0-beta.2": {
