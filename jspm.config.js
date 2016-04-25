b97220356753cf634d8c75f900a4af6be2bf306f
James Salas <j@jamesbs.com>
2016-04-24 22:14:19 -0400

    update versions

--- jspm.config.js
+++ jspm.config.js
@@ -38,28 +38,43 @@ SystemJS.config({
     "github:*/*.json"
   ],
   map: {
+    "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
+    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
     "os": "github:jspm/nodelibs-os@0.2.0-alpha",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
-    "ts": "github:frankwallis/plugin-typescript@4.0.5",
-    "typescript": "jspm_packages/npm/typescript@1.8.9%2Flib%2Ftypescript.js",
-    "angular2": "npm:angular2@2.0.0-beta.14",
+    "readline": "github:jspm/nodelibs-readline@0.2.0-alpha",
+    "ts": "github:frankwallis/plugin-typescript@4.0.7",
+    "typescript": "npm:typescript@1.8.10",
+    "angular2": "npm:angular2@2.0.0-beta.15",
     "es6-promise": "npm:es6-promise@3.1.2",
     "es6-shim": "npm:es6-shim@0.33.13",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
-    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.5",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.7",
     "reflect-metadata": "npm:reflect-metadata@0.1.2",
     "rxjs": "npm:rxjs@5.0.0-beta.2",
     "zone.js": "npm:zone.js@0.6.6"
   },
   packages: {
-    "github:frankwallis/plugin-typescript@4.0.5": {
+    "github:frankwallis/plugin-typescript@4.0.7": {
       "map": {
-        "typescript": "npm:typescript@1.8.9"
+        "typescript": "npm:typescript@1.8.10"
+      }
+    },
+    "github:jspm/nodelibs-buffer@0.2.0-alpha": {
+      "map": {
+        "buffer-browserify": "npm:buffer@4.6.0"
       }
     },
     "github:jspm/nodelibs-os@0.2.0-alpha": {
       "map": {
         "os-browserify": "npm:os-browserify@0.2.1"
+      }
+    },
+    "npm:buffer@4.6.0": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.2",
+        "ieee754": "npm:ieee754@1.1.6",
+        "isarray": "npm:isarray@1.0.0"
       }
     },
     "npm:es6-promise@3.1.2": {
